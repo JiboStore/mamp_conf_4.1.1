426cc173b891cc793d189b527ff89a614b07de21
Hakim Hauston <hakim.hauston@gmail.com>
2017-06-09 16:44:04 +0800

    Put the startup and shutdown command to script in case later forget. The URLs to access are as follows:
    http://10.6.0.18:8888
    https://10.6.0.18:9443

---
+++ startApache.sh
@@ -0,0 +1,4 @@
+#sudo /Applications/MAMP/Library/bin/apachectl restart
+#sudo /Applications/MAMP/Library/bin/apachectl stop
+#sudo /Applications/MAMP/Library/bin/apachectl start
+sudo ../Library/bin/apachectl start
